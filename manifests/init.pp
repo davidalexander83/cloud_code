c5fdf0266a1a288b3c17cce0a652696b21f5093e
davidalexander83 <david.alexander@puppet.com>
2017-04-28 08:58:27 +1000

    balsdf

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class cloud_code {
   azure_vm { 'myazurevm_ab454bd':
     ensure                      => present,
     location                    => 'australiasoutheast',
-    image => 'MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:latest,
+    image => 'MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:latest',
     user                        => 'puppet_user',
     password                    => 'Password123!',
     size                        => 'Standard_DS1_V2',
