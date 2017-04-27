f8b1ca76a92eb8ee7d15ceb4eb3d487539e95e3e
davidalexander83 <david.alexander@puppet.com>
2017-04-28 07:31:27 +1000

    aws update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 #Create azure vms
 
-class azure_code {
-  azure_vm { 'myvm':
+class cloud_code {
+  azure_vm { 'myazurevm':
     ensure         => present,
     location       => 'Australia Southeast',
     image          => 'Windows Server 2012 R2 Datacenter',
@@ -9,5 +9,17 @@ class azure_code {
     password       => 'Password123!',
     size           => 'DS1_V2',
     resource_group => 'davidalexander',
+  }
+
+  ec2_instance { 'myawsinstance':
+    ensure        => present,
+    region        => 'us-west-2',
+    image_id      => 'ami-09f47d69',
+    instance_type => 't2.small',
+    tags          => {
+      created_by => 'david.alexander',
+      department => 'tse',
+      project    => 'cloud-webinar-apj',
+    }
   }
 }
