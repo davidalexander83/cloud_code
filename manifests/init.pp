783e308b60db87007f987702fd9185646b441ae1
davidalexander83 <david.alexander@puppet.com>
2017-04-28 08:42:23 +1000

    sdf

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,24 +2,27 @@
 
 class cloud_code {
   ec2_instance { 'myawsinstance':
-    ensure        => present,
-    region        => 'us-west-2',
-    image_id      => 'ami-09f47d69',
-    instance_type => 't2.small',
-    subnet        => 'subnet-fdbb3198',
-    tags          => {
+    ensure          => present,
+    region          => 'us-west-2',
+    image_id        => 'ami-09f47d69',
+    instance_type   => 't2.small',
+    subnet          => 'subnet-fdbb3198',
+    security_groups => ['launch-wizard-3'],
+    tags            => {
       created_by => 'david.alexander',
       department => 'tse',
       project    => 'cloud-webinar-apj',
     }
   }
   azure_vm { 'myazurevm':
-    ensure         => present,
-    location       => 'australiasoutheast',
-    image          => 'Windows Server 2012 R2 Datacenter',
-    user           => 'puppet_user',
-    password       => 'Password123!',
-    size           => 'Standard_DS1_V2',
-    resource_group => 'davidalexander',
+    ensure                      => present,
+    location                    => 'australiasoutheast',
+    image                       => 'Windows Server 2012 R2 Datacenter',
+    user                        => 'puppet_user',
+    password                    => 'Password123!',
+    size                        => 'Standard_DS1_V2',
+    resource_group              => 'davidalexander',
+    public_ip_allocation_method => 'Dynamic',
+    public_ip_address_name      => 'mypublicIP',
   }
 }
