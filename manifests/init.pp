877b8dd8f14ae41c5c5caf7c463d52da1d1e95cf
davidalexander83 <david.alexander@puppet.com>
2017-04-28 08:55:35 +1000

    bal

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,10 @@
 #Create azure vms
 
 class cloud_code {
-  ec2_instance { 'myawsinstance':
-    ensure          => present,
-    region          => 'us-west-2',
-    image_id        => 'ami-09f47d69',
-    instance_type   => 't2.small',
-    subnet          => 'subnet-fdbb3198',
-    security_groups => ['sg-11cd4d6a'],
-    tags            => {
-      created_by => 'david.alexander',
-      department => 'tse',
-      project    => 'cloud-webinar-apj',
-    }
-  }
   azure_vm { 'myazurevm_ab454bd':
     ensure                      => present,
     location                    => 'australiasoutheast',
-    image                       => 'Windows Server 2012 R2 Datacenter',
+    image => 'MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:latest,
     user                        => 'puppet_user',
     password                    => 'Password123!',
     size                        => 'Standard_DS1_V2',
